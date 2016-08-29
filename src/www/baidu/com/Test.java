7089e08a87f5d97265cb52c091d4cac1e97ee95b
lihuimin1 <2427254954@qq.com>
2016-08-29 11:40:34 +0800

    0.0.6

--- src/www/baidu/com/Test.java
+++ src/www/baidu/com/Test.java
@@ -3,7 +3,7 @@ package www.baidu.com;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hellodddd");
 		System.out.println("sss");
 	}
 }
